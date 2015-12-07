7741683fa1f5c4d5b03923fcd49f293a772f242a
danjford <forddj93@gmail.com>
2015-12-07 22:27:34 +0000

    Removing helpers and putting them in their own repo

--- src/board.js
+++ src/board.js
@@ -1,10 +1,15 @@
 var board = [],
-  Helpers = require('./helpers.js');
+  Helpers = require('helpers.js');
 
 function Board() {
   createBoard();
 }
 
+
+/**
+ * Creates the a 9 square TicTacToe board
+ * @return {Void}, doesn't return anything
+ */
 function createBoard() {
 
   board = [];
@@ -17,6 +22,11 @@ function createBoard() {
 
 }
 
+
+/**
+ * Checks the rows to see if there is a winner
+ * @return {String|Boolean}, returns the player or false if nobody has one
+ */
 Board.prototype.checkRows = function() {
 
   for (var i = 0, ii = board.length; i < ii; i++) {
@@ -33,6 +43,10 @@ Board.prototype.checkRows = function() {
 
 };
 
+/**
+ * Checks the columns to see if there is a winner
+ * @return {String|Boolean}, returns the player or false if nobody has one
+ */
 Board.prototype.checkColumns = function() {
 
   for (var i = 0, ii = board.length; i < ii; i++) {
@@ -49,6 +63,10 @@ Board.prototype.checkColumns = function() {
 
 };
 
+/**
+ * Checks the diagonals to see if there is a winner
+ * @return {String|Boolean}, returns the player or false if nobody has one
+ */
 Board.prototype.checkDiagonals = function() {
 
   if ( (board[0][0] + board[1][1] + board[2][2]) === -3 || (board[0][2] + board[1][1] + board[2][0]) === -3) {
@@ -61,10 +79,20 @@ Board.prototype.checkDiagonals = function() {
 
 };
 
+/**
+ * Gets a position on the board.
+ * @return {Integer}, returns the player i.e. -1 or 1
+ */
 Board.prototype.getPosition = function(position) {
   return board[position[0]][position[1]];
 };
 
+/**
+ * Sets a player to the position on the board if the space is not taken.
+ * @param {Array} position, 'The coordinate at which the move should be set [row, col]'
+ * @param {String} player, 'X' or 'O'
+ * @return {Promise}
+ */
 Board.prototype.setPosition = function(position, player) {
 
   var _this = this;
@@ -85,6 +113,10 @@ Board.prototype.setPosition = function(position, player) {
 
 };
 
+/**
+ * Checks the board if it is in stale made
+ * @return {Boolean}, false if it is not in Stale Mate, else true.
+ */
 Board.prototype.staleMate = function() {
 
   for(var i = 0, ii = 3; i < ii; i++){
@@ -99,10 +131,18 @@ Board.prototype.staleMate = function() {
 
 };
 
+/**
+ * Gets a 'clone' of the board, modifying it won't affect the origianl
+ * @return {Array}, An array of coordinates i.e. the board
+ */
 Board.prototype.getBoard = function() {
-  return Helpers.merge([], board);
+  return Helpers.deepCopy([], board);
 };
 
+/**
+ * Returns the winner of the board or False.
+ * @return {String|Boolean}, Returns 'X', 'O' or false.
+ */
 Board.prototype.getWinner = function() {
 
   return this.checkRows() || this.checkColumns() || this.checkDiagonals();
